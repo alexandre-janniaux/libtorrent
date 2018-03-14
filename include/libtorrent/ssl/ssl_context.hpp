acd48a3eecf2c6186649cd8d653c821643cc6051
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2018-03-14 17:58:59 +0100

    WIP: Add boost functions

--- include/libtorrent/ssl/ssl_context.hpp
+++ include/libtorrent/ssl/ssl_context.hpp
@@ -3,21 +3,218 @@
 
 namespace libtorrent {
 
-namespace openssl {
-class SSLContext{
+namespace gnutls {
+class Context {
     public:
 
-    SSLContext_openssl() {
-        m_context = 
+    using error_code = boost::system::error_code;
+
+    // # typedefs
+    // file_format
+    // method
+    // native_handle_type
+    // options
+    // password_purpose
+
+    void add_certificate_authority(const const_buffer& ca) {
+        error_code ec;
+        add_certificate_authority(ca, ec);
+        // TODO: error check
     }
 
-};
-}
+    error_code add_certificate_authority(const buffer& ca, error_code& ec) {
 
+    }
 
-namespace gnutls {
-class SSLContext {
-    public:
+    void add_verify_path(const std::string& path) {
+        error_code ec;
+        add_verify_path(path, ec);
+        // TODO: error check
+    }
+
+    error_code add_verify_path(const std::string& path, error_code& ec) {
+
+    }
+
+    void clear_options(options o) {
+        error_code ec;
+        clear_options(o, ec);
+        // TODO: check errors
+    }
+
+    explicit Context(method m) {
+
+    }
+
+    Context(Conntext&& other) {
+
+    }
+
+    void load_verify_file(const std::string& filename) {
+        error_code ec;
+        load_verify_file(filename, ec);
+    }
+
+    error_code load_verify_file(const std::string& filename, error_code& ec) {
+
+    }
+
+    native_handle_type native_handle() {
+
+    }
+
+    Context& operator=(Context&& other) {
+
+    }
+
+    void set_default_verify_paths() {
+        error_code ec;
+        set_default_verify_paths(ec);
+    }
+
+    void set_default_verify_paths(error_code& ec) {
+
+    }
+
+    void set_options(options o) {
+        error_code ec;
+        set_options(o, ec);
+    }
+
+    template<typename PasswordCallback>
+    void set_password_callback(PasswordCallback callback) {
+        error_code ec;
+        set_password_callback(callback, ec);
+    }
+
+    template<typename PasswordCallback>
+    void set_password_callback(PasswordCallback& callback, error_code& ec) {
+
+    }
+
+    template<typename VerifyCallback>
+    void set_verify_callback(VerifyCallback& callback) {
+        error_code ec;
+        set_verify_callback(callback, ec);
+    }
+
+    template<typename VerifyCallback>
+    void set_verify_callback(VerifyCallback& callback, error_code& ec) {
+
+    }
+
+    void set_verify_depth(int depth) {
+        error_code ec;
+        set_verify_depth(depth, ec);
+    }
+
+    void set_verify_depth(int depth, error_code& ec) {
+
+    }
+
+    void set_verify_mode(verify_mode v) {
+        error_code ec;
+        set_verify_mode(v, ec);
+    }
+
+    void set_verify_mode(verify_mode v, error_code& ec) {
+
+    }
+
+    void use_certificate(const const_buffer& certificate, file_format format) {
+        error_code ec;
+        use_certificate(certificate, format, ec);
+    }
+
+    void use_certificate(const const_buffer& certificate, file_format format, error_code& ec) {
+
+    }
+
+    void use_certificate_chain(const const_buffer& chain) {
+        error_code ec;
+        use_certificate_chain(chain, ec);
+    }
+
+    void use_certificate_chain(const const_buffer& chain, error_code& ec) {
+
+    }
+
+    void use_certificate_chain_file(const std::string& filename) {
+        error_code ec;
+        use_certificate_chain_file(filename, ec);
+    }
+
+    void use_certificate_chain_file(const std::string& filename, error_code& ec) {
+
+    }
+
+    void use_certificate_file(const std::string& filename, file_format format) {
+        error_code ec;
+        use_certificate_file(filename, format, ec);
+    }
+
+    void use_certificate_file(const std::string& filename, file_format format, error_code& ec) {
+
+    }
+
+    void use_private_key(const const_buffer& private_key, file_format format) {
+        error_code ec;
+        use_private_key(private_key, format, ec);
+    }
+
+    void use_private_key(const const_buffer& private_key, file_format format, error_code ec) {
+
+    }
+
+    void use_private_key_file(const std::string& filename, file_format format) {
+        error_code ec;
+        use_private_key_file(filename, file_format, ec);
+    }
+
+    void use_private_key_file(const std::string& filename, file_format format, error_code& ec) {
+
+    }
+
+    void use_rsa_private_key(const const_buffer& private_key, file_format format) {
+        error_code ec;
+        use_rsa_private_key(private_key, format, ec);
+    }
+
+    void use_rsa_private_key(const const_buffer& private_key, file_format format, error_code& ec) {
+
+    }
+
+    void use_rsa_private_key_file(const std::string& filename, file_format format) {
+        error_code ec;
+        use_rsa_private_key_file(filename, file_format, ec);
+    }
+
+    void use_rsa_private_key_file(const std::string& filename, file_format format, error_code& ec) {
+
+    }
+
+    void use_tmp_dh(const const_buffer& dh) {
+        error_code ec;
+        use_tmp_dh(dh, ec);
+    }
+
+    void use_tmp_dh(const const_buffer& dh, error_code& ec) {
+
+    }
+
+    void use_tmp_dh_file(const std::string& filename) {
+        error_code ec;
+        use_tmp_dh_file(filename, ec);
+    }
+
+    void use_tmp_dh_file(const std::string& filename, error_code& ec) {
+
+    }
+
+    ~Context() {
+
+    }
+
 
     SSLContext_gnutls() {
         // Thread safe global init
