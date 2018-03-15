3ebaa8655449fd8beec8f6b20be5312c4e109048
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2018-03-15 14:51:52 +0100

    WIP: wrapper around openssl and gnutls X509 types and functions

--- include/libtorrent/ssl/ssl_certificate.hpp
+++ include/libtorrent/ssl/ssl_certificate.hpp
@@ -3,25 +3,102 @@
 
 namespace libtorrent {
 
+namespace openssl {
+
+class certificate {
+
+	public:
+	certificate() {}
+
+	static certificate from_mem_pem(string_view cert_data) {
+		certificate cert;
+
+		// wrap the PEM certificate in a BIO
+		auto bp = std::unique_ptr<BIO, BIO_free>(BIO_new_mem_buf(
+			const_cast<void*>(static_cast<void const*>(cert_data.data()))
+			, static_cast<int>(cert_data.size())));
+
+		// parse the certificate into OpenSSL's internal representation
+		cert.reset(PEM_read_bio_X509_AUX(bp, nullptr, nullptr, nullptr));
+
+		// we don't need the BIO anymore
+		bp.reset();
+
+		return std::move(cert);
+	}
+
+	X509* native_handle() {
+		return m_cert.get();
+	}
+
+	class general_names_t {
+
+		general_names_t(GENERAL_NAMES* names) {
+
+		}
+
+		friend general_names_t certificate::get_general_names();
+
+		public:
+		general_name_iterator begin() {
+
+		}
+
+		const_general_name_iterator begin() {
+
+		}
+	}
+
+	general_names_t get_subject_alt_names() {
+		auto* gens = static_cast<GENERAL_NAMES*>(
+			X509_get_ext_d2i(m_cert, NID_subject_alt_name, nullptr, nullptr));
+		return { gens };
+	}
+
+	private:
+	std::unique_ptr<X509, X509_free> m_cert;
+};
+}
 
 namespace gnutls {
 
-class SSLCertificate {
+class certificate {
 
-    public:
+	public:
+	using cert_type = gnutls_x509_crt_t;
 
-    SSLCertificate() {
-        gnutls_certificate_allocate_credentials(&m_cred);
-        gnutls_certificate_set_x509_system_trust(m_cred);
+	~certificate() {
+		gnutls_x509_crt_deinit(m_cert);
+	}
+
+	static certificate read_mem_pem(string_view cert_data) {
+		certificate cert;
+		// TODO: use gnutls_x509_crt_import(cert, datum*, fmt)
+		return cert;
+	}
+
+	cert_type native_handle() {
     }
 
-    gnutls_certificate_credentials_t get_native_handle() {
-        return m_cred;
-    }
+	subject_alt_names_t get_subject_alt_names() {
+		// TODO: use gnutls_x509_crt_get_subject_alt_name
+		subject_alt_names_t names(m_cert);
+		return std::move(names);
+	}
+
+	// TODO: iterators
+	subject_names_t get_subject_name() {
+	}
 
     private:
 
-    gnutls_certificate_credentials_t m_cred;
+	certificate() {
+		int ret = gnutls_x509_crt_init(&m_cert);
+		if(ret != GNUTLS_E_SUCCESS) {
+		}
+	}
+
+	gnutls_x509_crt_t m_cert;
 };
 
 }
--- include/libtorrent/ssl/ssl_store.hpp
+++ include/libtorrent/ssl/ssl_store.hpp
@@ -1,26 +1,70 @@
 #ifndef TORRENT_SSL_SSL_STORE_HPP
 #define TORRENT_SSL_SSL_STORE_HPP
 
+#ifdef LIBTORRENT_USE_GNUTLS
+#include <gnutls/gnutls.h>
+#endif
+
 namespace libtorrent {
+namespace ssl {
 
-namespace gnutls {
-class SSLStore {
-    public:
-    SSLStore_gnutls(){}
+#ifdef TORRENT_USE_OPENSSL
+namespace openssl {
+class store {
+	public:
+		store() {
+			m_store.reset(X509_STORE_new());
+			if(!m_store) {
+				// TODO: throw exception
+			}
+		}
 
-    private:
+		void add_cert(certificate& cert) {
+			X509_STORE_add_cert(m_store, certificate.native_handle());
+		}
 
-
+	private:
+		std::unique_ptr<X509_STORE, X509_STORE_free> m_store;
 };
 }
+#endif
+
+#ifdef TORRENT_USE_GNUTLS
+namespace gnutls {
+class store {
+	public:
+		store() {
+			m_cred_init = gnutls_certificate_allocate_credentials(&m_cred);
+			if(m_crend_init != GNUTLS_E_SUCCESS) {
+				// throw
+			}
+		}
+
+		~store() {
+			gnutls_certificate_free_credentials(m_cred);
+		}
+
+		void add_cert(certificate& cert) {
+
+		}
+
+	private:
+
+	gnutls_credential_t m_cred;
+	int m_cred_init = -1;
+	int m_gnutls_init = -1;
+};
+}
+#endif
 
 #if defined TORRENT_USE_OPENSSL
 #error openssl::SSLStore Not implemented
-using openssl::SSLStore;
+using openssl::store;
 #elif defined TORRENT_USE_GNUTLS
-using gnutls::SSLStore;
+using gnutls::store;
 #endif
 
-}
+} // namespace ssl
+} // namespace libtorrent
 
 #endif
